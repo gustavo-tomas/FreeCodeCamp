0d32e3a4244a97d6023acc24afcca9f98e280046
guga <guga.tomas10@gmail.com>
2021-07-10 12:31:41 -0300

    added markdown editor

--- FrontEndDevelopmentLibraries/MarkdownPreviewer/scripts.js
+++ FrontEndDevelopmentLibraries/MarkdownPreviewer/scripts.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+function getTextArea() {
+    var txt = $('#editor').val();
+    document.getElementById('preview').innerHTML = marked(txt);
+}
+
+function initialText() {
+    var editorText = 'luiz eh muito gay';
+    document.getElementById('editor').innerHTML = editorText;
+    document.getElementById('preview').innerHTML = marked(editorText);
+}
